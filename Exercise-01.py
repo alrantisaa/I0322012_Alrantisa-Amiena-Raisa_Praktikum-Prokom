38e70b6e048e8fe4b7da0634204476c887a8d84f
alrantisaa <alrantisaar@student.uns.ac.id>
2023-03-13 12:34:55 +0700

    Exercise-01

---
+++ Exercise-01.py
@@ -0,0 +1,20 @@
+#Tugas 1 Praktikum PROKOM
+
+print("~"*55)
+print("\n"+"Nama : Alrantisa Amiena Raisa")
+print("NIM : I0322012"+"\n")
+print("~"*55 + "\n")
+
+print("""Perkenalkan, nama saya Alrantisa Amiena Raisa dengan NIM I0322012. 
+Semua orang memanggil saya  dengan panggilan Ranti. Saya lahir di Tangerang 
+pada tanggal 21 Agustus 2004. Kota Tangerang merupakan tempat di mana saya 
+dilahirkan dan dibesarkan. Kota Tangerang memiliki makanan khas, yaitu Laksa.
+Di Kota Tangerang terdapat pusat kuliner yang terkenal yaitu Pasar Lama.
+Di Kota Tangerang, saya memiliki banyak kenangan-kenangan yang indah untuk 
+dikenang dan di sana saya belajar banyak sekali hal tentang hidup. 
+
+Setelah lulus SMA, saya pergi merantau ke Kota Surakarta. Saya berkuliah di 
+jurusan Teknik Industri Universitas Sebelas Maret. Saya memilih jurusan Teknik
+Industri karena saya merasa memiliki kompetensi di jurusan tersebut dan jurusan
+tersebut memiliki prospek kerja yang sangat luas karena jurusan tersebut 
+mempelajari banyak hal.""") 
