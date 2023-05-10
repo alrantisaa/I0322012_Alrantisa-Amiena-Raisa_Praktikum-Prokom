5c930e0491e228abb278255efc1c8efde9cb18b1
alrantisaa <alrantisaar@student.uns.ac.id>
2023-05-10 19:20:07 +0700

    tugas6

---
+++ ho.py
@@ -0,0 +1,149 @@
+# # # # def luas_segitiga(alas, tinggi):
+# # # #     luas = (alas * tinggi) / 2
+# # # #     print("="*55)
+# # # #     print ("Luas segitiga: %f" %luas)
+
+# # # # def Luas_persegi(alas, tinggi)
+# # # #     LuasPersegi = alas * tinggi
+# # # #     print("="*55)
+# # # #     print ("Luas persegi: %f" %luas)
+
+# # # # alas = int(input("Inputkan nilai alas       : "))
+# # # # tinggi = int(input("Inputkan nilai tinggi   : "))
+# # # # luas_segitiga(alas, tinggi)
+# # # # Luas_persegi(alas, tinggi)
+
+# # # # program utama
+# # # #input data 
+# # # alas = int(input("masukkan nilai alas       : "))
+# # # tinggi = int(input("masukkan nilai tinggi   : "))
+
+# # # #output prog
+
+# # # luas_segitiga(alas, tinggi)
+
+# # def luas_persegi (alas, tinggi) :
+# #     luas = (sisi * sisi)
+# #     print ("="*55)
+# #     print ("Luas persegi: %f" %luas)
+
+# # sisi = int(input("inputkan nilai sisi persegi:"))
+# # luas_persegi(sisi, sisi)
+
+# # def luas_kubus(sisi):
+# #     luaskubus=sisi*sisi
+# #     return luaskubus
+
+# # def volume_kubus(sisi):
+# #     volume = luas_kubus(sisi)* sisi
+# #     return volume
+# #     print("="*55)
+# #     print("volume kubus = %d" % volume_kubus(sisi))
+# # volume_kubus(sisi)
+# # print("="*55)
+# # print("volume kubvus = %f" % volume_kubus(sisi))
+
+
+
+# # bunga = ["anggrek dendro", "anggrek bulan", "anggrek ekor tupai", "anggrek golden shower", "anggrek katlya"]
+# # # fungsi untuk menampilkan data base
+# # def tampil_data
+# #     print ("daftar bunga yang ada ")
+# #     if len(bunga)==0:
+# #         print(" belum ada data bunga ")
+# #     else
+# #         for indeks in range(0, len(bunga)):
+# #             print("no = ", indeks, " ", bunga[indeks])
+
+# # # fungsi untuk masukkan data baru
+
+# # def insert_data():
+# #     bunga_baru = input("masukkan nama bunga baru = ")
+# #     bunga.append(bunga_baru)
+# #     tampil_data()
+
+
+
+# # #program utama database
+
+# # print("====Selamat datang di prog data base ====")
+# # print()
+
+# # print(" [1] menu show data bunga ")
+# # print(" [2] masukkan data bunga  ")
+
+# # print()
+# # print("="*55)
+
+# # pilih = input("masukkan pilihan menu = ")
+# # print(" pilihan anda adalah          = ", pilih)
+
+# # if pilih == "1"
+# #     tampil_data()
+# # if pilih == "2"
+# #     insert_data()
+
+# # print(20 * "=")
+# # print("Kalkulator Sederhana")
+# # print(20 * "=" + "\n")
+
+# # angka_1 = float(input("Masukkan angka 1 = "))
+# # angka_2 = float(input("Masukkan angka 2 = "))
+# # operator = input("operator (+, -, x, /) : ")
+
+# # if operator == "+":
+# #     hasil = angka_1 + angka_2
+# #     print(f"Hasilnya adalah {hasil}")
+# # elif operator == "-":
+# #     hasil = angka_1 - angka_2
+# #     print(f"Hasilnya adalah {hasil}")
+# # elif operator == "x":
+# #     hasil = angka_1 * angka_2
+# #     print(f"Hasilnya adalah {hasil}")
+# # elif operator == "/":
+# #     hasil = angka_1 / angka_2
+# #     print(f"Hasilnya adalah {hasil}")
+
+# # for i in range(1,100):
+# #     i += 1
+# #     print(i)
+
+# # angka = 6
+# # while angka > 5:
+# #     print("al kangen nadhira")
+
+# # for i in range(1,100):
+# #     i += 1
+# #     print(i)
+
+# # kalimat = str(input("Masukkan kalimat yang Anda inginkan: "))
+# # for urut in kalimat:
+# #     print(urut)
+
+# # angka = int(input("Masukkan angka yang Anda inginkan: "))
+# # if angka % 2 == 0:
+# #     print("Angka yang Anda masukkan adalah", angka, ",", angka, "adalah bilangan genap.")
+# # else:
+# #     print("Angka yang Anda masukkan adalah", angka, ",", angka, "adalah bilangan ganjil.")
+
+# angka = int(input("Masukkan angka yang Anda inginkan: "))
+# if angka % 2 == 0:
+#     print("Angka yang Anda masukkan adalah", angka, ",", angka, "adalah bilangan genap.")
+# else:
+#     print("Angka yang Anda masukkan adalah", angka, ",", angka, "adalah bilangan ganjil.")
+
+# print("Halo!")
+# nama = input("Siapa nama kamu? ")
+# print("Okay, halo", nama, "!")
+# password = "nadhira sayang ranti tapi ranti nya ngga"
+# if pwd == password:
+#     print("access granted!")
+# else:
+#     print("sorry, access denied")
+
+# print("Halo, ", nama,". Semangat hidup ya bang, hidup emang keras bang, tapi ya jalanin aja bang.")
+# print("Jangan lupa makan yang bener ya ngab, jangan begadang mulu ngab tar cepet mati")
+# print("Jangan lupa juga buat cari kebahagiaan buat diri lu sendiri bang")
+# print("Gue seneng bang kalo liat lu seneng juga walaupun gue cuma liat di story ig lu bang")
+# print("FYI juga nih bang, gue adalah one of nadhira's archives viewers bang")
+# print("intinya gue seneng banget kalo liat lu happy")
